59ffc11ee1f3c44f61bf309aba5226f8ce741efb
Leon Prenkocevic <leonprenkocevic40@gmail.com>
2022-08-06 18:14:25 +0200

    changed function so as to return result and not directly send it to console

--- app.js
+++ app.js
@@ -9,27 +9,27 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if( playerSelection == "rock" && computerSelection == "rock"){
-        console.log("Draw!")
+        return "Draw!"
     }else if(playerSelection == "rock" && computerSelection == "paper"){
-        console.log("You Lose! Paper beats Rock!")
+        return "You Lose! Paper beats Rock!"
     }else if(playerSelection == "rock" && computerSelection == "scissors"){
-        console.log("You Win! Rock beats Scissors!")
+        return "You Win! Rock beats Scissors!"
     }else if(playerSelection == "paper" && computerSelection == "paper"){
-        console.log("Draw!")
+        return "Draw!"
     }else if(playerSelection == "paper" && computerSelection == "scissors"){
-        console.log("You Lose! Scissors beat Paper!")
+        return "You Lose! Scissors beat Paper!"
     }else if(playerSelection == "paper" && computerSelection == "rock"){
-        console.log("You Win! Paper beats Rock!")
+        return "You Win! Paper beats Rock!"
     }else if(playerSelection == "scissors" && computerSelection == "scissors"){
-        console.log("Draw!")
+        return "Draw!"
     }else if(playerSelection == "scissors" && computerSelection == "paper"){
-        console.log("You Win! Scissors beat Paper!")
+        return "You Win! Scissors beat Paper!"
     }else if(playerSelection == "scissors" && computerSelection == "rock"){
-        console.log("You Lose! Rock beats Scissors!")
+        return "You Lose! Rock beats Scissors!"
     }
 }
 
 let comp_choice = getComputerChoice();
 let player_choice = prompt("Enter choice: ").toLocaleLowerCase()
 
-playRound(player_choice, comp_choice)
+console.log(playRound(player_choice, comp_choice))
